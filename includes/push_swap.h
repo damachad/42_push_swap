f180823b2b611f0e451adfb88c251d0b0ecb836f
David Correia <davidmachado98@hotmail.com>
2023-08-10 17:07:46 +0100

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: damachad <damachad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 10:16:42 by damachad          #+#    #+#             */
-/*   Updated: 2023/08/10 14:56:29 by damachad         ###   ########.fr       */
+/*   Updated: 2023/08/10 16:51:25 by damachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,20 @@ bool		exist_duplicates(t_stack *nbrs);
 bool		valid_range(char *str);
 long		long_atoi(char *nptr);
 
-/*------------------------operations------------------------*/
+/*---------------------------init---------------------------*/
 t_stack		*new_node(int data);
-void		push(t_stack **top, int data);
+void		push_new_data(t_stack **top, int data);
+
+/*------------------------operations------------------------*/
+void		swap(t_stack **top);
+void		rotate(t_stack **top);
+void		rev_rotate(t_stack **top);
+void		push_to_b(t_stack **top_a, t_stack **top_b);
+
+/*---------------------------stack---------------------------*/
 bool		is_sorted(t_stack *stack);
+t_stack		*last_node(t_stack *top);
+t_stack 	*second_last_node(t_stack *top);
 
 /*---------------------------clean---------------------------*/
 void		error(t_stack *stack);
