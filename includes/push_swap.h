0f47ed29de2855591d1102a9c6f818409c7e4326
David Correia <davidmachado98@hotmail.com>
2023-08-15 10:29:34 +0100

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: damachad <damachad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 10:16:42 by damachad          #+#    #+#             */
-/*   Updated: 2023/08/10 16:51:25 by damachad         ###   ########.fr       */
+/*   Updated: 2023/08/14 14:56:00 by damachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,17 +34,33 @@ long		long_atoi(char *nptr);
 /*---------------------------init---------------------------*/
 t_stack		*new_node(int data);
 void		push_new_data(t_stack **top, int data);
+void		push_swap(t_stack **stack_a, t_stack **stack_b, size_t size);
 
 /*------------------------operations------------------------*/
 void		swap(t_stack **top);
 void		rotate(t_stack **top);
 void		rev_rotate(t_stack **top);
 void		push_to_b(t_stack **top_a, t_stack **top_b);
+void		push_to_a(t_stack **top_a, t_stack **top_b);
+
+/*------------------------operations2------------------------*/
+void		ss(t_stack **stack_a, t_stack **stack_b);
+void		rr(t_stack **stack_a, t_stack **stack_b);
+void		rrr(t_stack **stack_a, t_stack **stack_b);
+
+/*-------------------------operator-------------------------*/
+void	process_a(void (*operation)(t_stack**), t_stack **stack_a);
+void	process_b(void (*operation)(t_stack**), t_stack **stack_b);
+void	process_c(void (*op)(t_stack**, t_stack**), t_stack **a, t_stack **b);
+
+/*----------------------------sort----------------------------*/
+void	sort_3(t_stack **stack_a);
 
 /*---------------------------stack---------------------------*/
 bool		is_sorted(t_stack *stack);
 t_stack		*last_node(t_stack *top);
-t_stack 	*second_last_node(t_stack *top);
+t_stack		*second_last_node(t_stack *top);
+size_t		stack_size(t_stack *top);
 
 /*---------------------------clean---------------------------*/
 void		error(t_stack *stack);
