6a150d04201ecf67533a9779e3f1e21610476057
David Correia <davidmachado98@hotmail.com>
2023-08-16 16:07:55 +0100

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: damachad <damachad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 10:16:42 by damachad          #+#    #+#             */
-/*   Updated: 2023/08/15 11:00:00 by damachad         ###   ########.fr       */
+/*   Updated: 2023/08/16 15:40:03 by damachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@
 typedef struct s_stack
 {
 	int					val;
+	unsigned int		cur_position;
+	unsigned int		move_cost;
+	bool				up_median;
+	bool				cheapest;
+	struct s_stack		*target;
 	struct s_stack		*next;
 }						t_stack;
 
@@ -35,35 +40,46 @@ long		long_atoi(char *nptr);
 t_stack		*new_node(int val);
 void		push_new_val(t_stack **top, int val);
 void		push_swap(t_stack **stack_a, t_stack **stack_b, size_t size);
+t_stack		*highest(t_stack *stack);
 
 /*------------------------operations------------------------*/
 void		swap(t_stack **top);
 void		rotate(t_stack **top);
 void		rev_rotate(t_stack **top);
-void		push_to_b(t_stack **top_a, t_stack **top_b);
-void		push_to_a(t_stack **top_a, t_stack **top_b);
+void		push_to(t_stack **top_1, t_stack **top_2, char list);
 
 /*------------------------operations2------------------------*/
 void		ss(t_stack **stack_a, t_stack **stack_b);
 void		rr(t_stack **stack_a, t_stack **stack_b);
 void		rrr(t_stack **stack_a, t_stack **stack_b);
-
-/*-------------------------operator-------------------------*/
-void	process_a(void (*operation)(t_stack**), t_stack **stack_a);
-void	process_b(void (*operation)(t_stack**), t_stack **stack_b);
-void	process_c(void (*op)(t_stack**, t_stack**), t_stack **a, t_stack **b);
+void		do_op(void (*operation)(t_stack**), t_stack **stack_a, char list);
 
 /*----------------------------sort----------------------------*/
-void	sort_3(t_stack **stack_a);
+void		sort_3(t_stack **stack_a);
+void		big_sort(t_stack **a, t_stack **b, size_t size);
+void		move_nodes(t_stack **a, t_stack **b);
+void		move_top(t_stack **a, t_stack **b, t_stack *cheapest, \
+			void (*rot)(t_stack**, t_stack**));
+void		finish_rot(t_stack **stack, t_stack *top, char list);
+
+/*----------------------------cost----------------------------*/
+void		set_cur_position(t_stack *stack);
+void		set_targets(t_stack *a, t_stack *b);
+void		set_cost(t_stack *a, t_stack *b);
+t_stack		*set_cheapest(t_stack *b);
+void		calculate_costs(t_stack *a, t_stack *b);
 
 /*---------------------------stack---------------------------*/
 bool		is_sorted(t_stack *stack);
 t_stack		*last_node(t_stack *top);
 t_stack		*second_last_node(t_stack *top);
 size_t		stack_size(t_stack *top);
+t_stack		*smallest(t_stack *stack);
 
 /*---------------------------clean---------------------------*/
 void		error(t_stack *stack);
 void		free_stack(t_stack *top);
+
+void	print_stack(t_stack *stack, char c);
 
 #endif
